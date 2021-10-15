0df83e4b4f4bdd6e012fb9628ffc58ba970429c0
JustifiedFlaw <ebbytes@gmail.com>
2021-10-15 12:22:13 -0400

    removed copied and pasted access token

--- WrBot/RestEase/Interfaces/ISrcApi.cs
+++ WrBot/RestEase/Interfaces/ISrcApi.cs
@@ -6,8 +6,6 @@ namespace RestEase.Interfaces
 {
     [BasePath("v1")]
     [Header("User-Agent", "RestEase")]
-    [Header("Authorization", "Bearer txeqhs9wab81yv9hwl84mh1rw8rg4j")]
-    [Header("Client-Id", "a6423urau9t5wwb06seg4pj6ptm3q3")]
     public interface ISrcApi
     {
         [Get("leaderboards/{gameId}/category/{categoryId}?top=1")]

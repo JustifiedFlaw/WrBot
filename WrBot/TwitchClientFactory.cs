f85a84cc1695e77449f44d223e8d005a67a3a0ee
JustifiedFlaw <ebbytes@gmail.com>
2021-11-27 08:41:18 -0500

    allow messages every 5 seconds

--- WrBot/TwitchClientFactory.cs
+++ WrBot/TwitchClientFactory.cs
@@ -12,14 +12,15 @@ public static class TwitchClientFactory
         var clientOptions = new ClientOptions
             {
                 MessagesAllowedInPeriod = 750,
-                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+                ThrottlingPeriod = TimeSpan.FromSeconds(5)
             };
         var customClient = new WebSocketClient(clientOptions);
         var client = new TwitchClient(customClient);
 
         var credentials = new ConnectionCredentials(settings.BotName, settings.AccessToken);
 
-        client.Initialize(credentials, settings.Channels.Select(c => c.Name).ToList());
+        var channelList = settings.Channels.Select(c => c.Name).ToList();
+        client.Initialize(credentials, channelList);
 
         client.Connect();
 
